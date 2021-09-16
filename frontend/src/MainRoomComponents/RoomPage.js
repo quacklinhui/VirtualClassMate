6b525253d341b043f9743dd2d141ec64209a3e2e
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-09-17 02:16:54 +0800

    a bit of styling

--- frontend/src/MainRoomComponents/RoomPage.js
+++ frontend/src/MainRoomComponents/RoomPage.js
@@ -3,6 +3,7 @@ import NavBar from '../HomeComponents/NavBar/NavBar';
 import {Button, Typography, Paper, Box, Container} from '@material-ui/core'
 import {useState} from 'react';
 import {BrowserRouter as Router,  useHistory} from "react-router-dom";
+import ToDoList from './ToDoList/ToDoList';
 
 // To do: the number of buttons should be dynamically formed based on the database
 function RoomPage() {
@@ -19,9 +20,9 @@ let history = useHistory();
             <Typography> You are in: "Insert RoomName"</Typography>
             <Button style={{position:"absolute", right: 20,alignSelf: "flex-end", backgroundColor: "orange"}} onClick={() => { history.push("/home")}}>Back to HomePage</Button>
           </div>
-          <div style ={{display: "flex",flexDirection: 'row',height: 30,paddingTop: 5, width: "70%"}}>
-            <Container style={{width:"45%", backgroundColor: "#D8ABEC60"}}>
-                <Button>Personal</Button>
+          <div style ={{display: "flex",flexDirection: 'row',height: 30,paddingTop: 5, width: "70%", height: 400}}>
+            <Container style={{width:"45%", backgroundColor: "#D8ABEC60",}}>
+                <Container style ={{paddingTop: 10}}>Personal <ToDoList></ToDoList></Container>
             </Container>
             <Container style={{width:"45%", backgroundColor: "#D8ABEC60"}}><Button>Group</Button></Container>
             <Container style={{width:"25%", backgroundColor: "#D8ABEC60", position: "absolute", right: 20}}>
--- frontend/src/MainRoomComponents/ToDoList/ToDoList.js
+++ frontend/src/MainRoomComponents/ToDoList/ToDoList.js
@@ -1,31 +1,13 @@
 
-import NavBar from './NavBar/NavBar';
+
 import {Button, Typography, Paper, Box, Container} from '@material-ui/core'
-import CreateRoomPopUp from './CreateRoomPopUp/CreateRoomPopUp';
-import {useState} from 'react';
-import {BrowserRouter as Router,  useHistory} from "react-router-dom";
-import RoomPage from '../MainRoomComponents/RoomPage';
 
 // To do: the number of buttons should be dynamically formed based on the database
 function ToDoList() {
-  let history = useHistory();
-  const [createPopUp, setCreatePopUp] = useState(false); //set the default state to false
-    
   return (
       <div>
-      <main>
-      <div>
-        <NavBar />
-      </div>
-        <Container color = "primary.main" maxWidth="lrg">
-          <Typography>
-              Your Rooms
-          </Typography>
-          <Button variant="contained" color="primary" onClick={() => { history.push("/room")}}>
-              Temp Enter Room
+          <Button> Hello
           </Button>
-        </Container>
-        </main>
 
       </div>
     );
