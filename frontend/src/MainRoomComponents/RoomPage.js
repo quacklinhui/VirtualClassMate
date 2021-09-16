16f2ca2af18d2cb1c37ae529c2cc44385b025ec5
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-09-16 21:34:46 +0800

    create room pop up

--- frontend/src/MainRoomComponents/RoomPage.js
+++ frontend/src/MainRoomComponents/RoomPage.js
@@ -3,7 +3,7 @@ import {Button, Typography, Paper, Box, Container} from '@material-ui/core'
 import CreateRoomPopUp from './CreateRoomPopUp/CreateRoomPopUp';
 import {useState} from 'react';
 // To do: the number of buttons should be dynamically formed based on the database
-function HomePage() {
+function RoomPage() {
   const [createPopUp, setCreatePopUp] = useState(false); //set the default state to false
     
   return (
@@ -29,4 +29,4 @@ function HomePage() {
     );
   }
   
-  export default HomePage;
+  export default RoomPage;
