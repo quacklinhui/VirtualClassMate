df2d21f907e0c0276c4390dd2c480ea72f8b744e
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-09-16 23:30:15 +0800

    navigate from the room page to the homepage

--- frontend/src/MainRoomComponents/RoomPage.js
+++ frontend/src/MainRoomComponents/RoomPage.js
@@ -1,9 +1,11 @@
 import NavBar from '../HomeComponents/NavBar/NavBar';
 import {Button, Typography, Paper, Box, Container} from '@material-ui/core'
 import {useState} from 'react';
+import {BrowserRouter as Router,  useHistory} from "react-router-dom";
 
 // To do: the number of buttons should be dynamically formed based on the database
 function RoomPage() {
+let history = useHistory();
   const [createPopUp, setCreatePopUp] = useState(false); //set the default state to false
     
   return (
@@ -14,7 +16,7 @@ function RoomPage() {
       <Container style={{alignItems:"center", alignContent:"center",textAlign: "center",}}>
         <div style ={{display: "flex",flexDirection: 'row',height: 30,paddingTop: 20, justifyContent:"center"}}>
             <Typography> You are in: "Insert RoomName"</Typography>
-            <Button style={{position:"absolute", right: 20,alignSelf: "flex-end", backgroundColor: "orange"}}>Back to HomePage</Button>
+            <Button style={{position:"absolute", right: 20,alignSelf: "flex-end", backgroundColor: "orange"}} onClick={() => { history.push("/home")}}>Back to HomePage</Button>
           </div>
           <div style ={{display: "flex",flexDirection: 'row',height: 30,paddingTop: 5, width: "70%"}}>
             <Container style={{width:"45%", backgroundColor: "#D8ABEC60"}}>
