ac0768a0137e826fde6ed2f7fe43d2bbc5dc9f3a
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-09-16 18:47:32 +0800

    installing libraries

--- frontend/src/HomeComponents/HomePage.js
+++ frontend/src/HomeComponents/HomePage.js
@@ -1,10 +1,17 @@
 import NavBar from './NavBar/NavBar';
+import {Button} from '@material-ui/core'
 
 function HomePage() {
     return (
+      <>
       <div>
         <NavBar />
       </div>
+
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      </>
     );
   }
   
