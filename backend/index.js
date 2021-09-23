cd199bf68b37b16f9a18475fde7ee4b8eb7cdd2b
Amadeus Koh <60766014+amadeuskyj@users.noreply.github.com>
2021-09-23 11:42:32 +0800

    Connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,14 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-app.use(express.json({limit: "30mb", extended: true}));
-app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({limit: "30mb", extended: true})); // replace bodyParser
+app.use(express.urlencoded({limit: "30mb", extended: true})); // replace bodyParser
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://virtualclassmate:virtualclassmate@cluster.dnmqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// Connect Mongoose to DB
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) // When connection is successful
+    .catch((error) => console.log(error.message)); // When connection is not successful
