d2c74ff633f5ff3b6e98f1ac4bd8900a685b1ba0
C190194 <73808832+C190194@users.noreply.github.com>
2021-09-28 10:48:44 +0800

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,16 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    toDoList: [{type: Schema.Types.ObjectId, ref: 'ToDoItem'}],
-    level: Number,
-    experience: Number,
-    rooms: [{type: Schema.Types.ObjectId, ref: 'Room'}],
-    avatarID: String,
-    friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
+  //username: String,
+  password: String,
+  email: String,
+  toDoList: [{ type: mongoose.Schema.Types.ObjectId, ref: "ToDoItem" }],
+  level: Number,
+  experience: Number,
+  rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "Room" }],
+  avatarID: String,
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
