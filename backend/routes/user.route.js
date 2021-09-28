a243fb046bf58b816e9064d0a24cde9960e5365f
C190194 <73808832+C190194@users.noreply.github.com>
2021-09-29 00:44:58 +0800

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,17 +2,32 @@ import express from "express";
 
 import {
   getUsers,
+  getUser,
   register,
   login,
   deleteUser,
   updateAvatar,
+  searchUsers,
+  addFriend,
+  deleteFriend,
+  createRoom,
+  deleteRoom,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUser);
 router.post("/", register);
 router.post("/login", login);
 router.delete("/:id", deleteUser);
+
 router.patch("/avatar/:id", updateAvatar);
+
+router.get("/friend/:friend_name", searchUsers);
+router.patch("/addFriend/:id", addFriend);
+router.patch("/deleteFriend/:id", deleteFriend);
+
+router.patch("/createRoom/:id", createRoom);
+router.patch("/deleteRoom/:id", deleteRoom);
 export default router;
