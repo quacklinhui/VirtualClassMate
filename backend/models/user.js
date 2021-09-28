b78458ac896c4e4577469386c5535d620703c00d
C190194 <73808832+C190194@users.noreply.github.com>
2021-09-29 00:44:23 +0800

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,9 +8,11 @@ const userSchema = mongoose.Schema({
   level: Number,
   experience: Number,
   rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "Room" }],
-  avatarID: String,
+  avatarID1: String,
+  avatarID2: String,
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
+userSchema.index({ username: "text" });
 const User = mongoose.model("User", userSchema);
 export default User;
