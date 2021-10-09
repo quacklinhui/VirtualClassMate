d1bb0af1f27caa8323f652bc664a31a1758866a9
quacklinhui <61533311+quacklinhui@users.noreply.github.com>
2021-10-09 23:27:06 +0800

    change z index for dark purple part

--- frontend/src/MainRoomComponents/InviteFriends/InviteFriendsPopUp.js
+++ frontend/src/MainRoomComponents/InviteFriends/InviteFriendsPopUp.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './InviteFriendsPopUp.css'
-
-
+import CloseIcon from '@material-ui/icons/Close';
+import {Button, TextField,TextareaAutosize, IconButton} from '@material-ui/core'
 function InviteFriendsPopUp(props) {
     return (
         <div className = "InviteFriendPopUp">
             <div className = "popup-inner">   
-                <button className = "closePopUp" variant="contained" onClick={props.handleClose}>x</button>
+                <IconButton className = "closePopUp" onClick={props.handleClose}>
+                    <CloseIcon/>
+                </IconButton>
                 <h1>Invite Friends</h1>
+                {props.children}
             </div>
         </div>
     );
